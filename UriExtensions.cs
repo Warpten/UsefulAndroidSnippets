31e2cdb6ed5f5667629b41ef1516d75040b62f2c
Warpten <vertozor@gmail.com>
2017-08-26 14:44:39 +0200

    Update UriExtensions.cs

--- UriExtensions.cs
+++ UriExtensions.cs
@@ -1,9 +1,6 @@
 using System;
 using System.ComponentModel;
 using System.Text;
-using Deezy.Cryptography.Engines;
-using Deezy.Cryptography.Paddings;
-using Deezy.Cryptography.Parameters;
 using System.Collections.Generic;
 using System.Linq;
 using System.IO;
