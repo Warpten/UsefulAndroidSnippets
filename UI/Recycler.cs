f92a813b08890454b4fa9c30b1b134c2d1abc5e0
Warpten <vertozor@gmail.com>
2017-08-26 14:52:09 +0200

    Create Recycler.cs

---
+++ UI/Recycler.cs
@@ -0,0 +1,26 @@
+using Android.Support.V7.Widget;
+
+namespace Warpten.UI
+{
+    public sealed class Recycler<T> where T : RecyclerView.Adapter
+    {
+        public RecyclerView View { get; }
+
+        public T Adapter
+        {
+            get => View.GetAdapter() as T;
+            set => View.SetAdapter(value);
+        }
+
+        public RecyclerView.LayoutManager LayoutManager
+        {
+            get => View.GetLayoutManager();
+            set => View.SetLayoutManager(value);
+        }
+
+        public Recycler(RecyclerView view)
+        {
+            View = view;
+        }
+    }
+}
